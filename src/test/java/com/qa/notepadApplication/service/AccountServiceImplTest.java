9df2f86893e4dab3583d031c891a5e1d259e61b5
fortunexfortune <osadolor.fortune@yahoo.it>
2019-04-04 15:10:43 +0100

    added findByAccountID junit test

---
+++ src/test/java/com/qa/notepadApplication/service/AccountServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.qa.notepadApplication.service;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.qa.notepadApplication.persistence.domain.Account;
+import com.qa.notepadApplication.persistence.repository.AccountRepository;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+class AccountServiceImplTest {
+	
+	@Mock
+	private AccountRepository repo;
+	@InjectMocks
+	private AccountServiceImpl accountService;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+//	@Test
+//	void testAddAccount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testGetAllAccounts() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testDeleteAccount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testUpdateAccount() {
+//		fail("Not yet implemented");
+//	}
+
+	@Test
+	void testFindAccountByID() {
+		Account account = new Account();
+		account.setUserName("fortune");
+		account.setPwd("1234");
+		Optional<Account>accountOptional = Optional.of(account);
+		when(repo.findById(anyString()) ).thenReturn(accountOptional);
+		
+		Optional<Account> accountinDB = accountService.findAccountByID("fortune");
+		assertNotNull(accountinDB);
+		assertEquals("fortune", accountinDB.get().getUserName());
+	}
+
+}
